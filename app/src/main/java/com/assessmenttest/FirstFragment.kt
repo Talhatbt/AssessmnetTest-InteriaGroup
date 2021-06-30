f244395d36057389986d9b0392cdc94f89ae4595
talhabintariq <talha2tbt@hotmail.com>
2021-06-30 21:30:32 +0500

    Assessment test development completed

--- app/src/main/java/com/assessmenttest/FirstFragment.kt
+++ app/src/main/java/com/assessmenttest/FirstFragment.kt
@@ -20,7 +20,6 @@ class FirstFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_travelling_detail, container, false)
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
