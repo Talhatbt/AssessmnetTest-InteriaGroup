0aaf054c74211891fee504427e4cc2711935926a
talhabintariq <talha2tbt@hotmail.com>
2021-07-08 22:54:39 +0500

    Removed unused code

--- app/src/main/java/com/assessmenttest/database/AppDatabase.kt
+++ app/src/main/java/com/assessmenttest/database/AppDatabase.kt
@@ -7,7 +7,6 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.assessmenttest.models.TravellingData
-import java.io.*
 
 
 @Database(entities = [TravellingData::class], version = 1)
@@ -23,7 +22,11 @@ abstract class AppDatabase : RoomDatabase() {
             if (INSTANCE == null) {
                 synchronized(AppDatabase::class.java) {
                     if (INSTANCE == null) {
-                        INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, DB_NAME)
+                        INSTANCE = Room.databaseBuilder(
+                            context.applicationContext,
+                            AppDatabase::class.java,
+                            DB_NAME
+                        )
                             //.allowMainThreadQueries() // Uncomment if you don't want to use RxJava or coroutines just yet (blocks UI thread)
                             .addCallback(object : Callback() {
                                 override fun onCreate(db: SupportSQLiteDatabase) {
@@ -36,36 +39,6 @@ abstract class AppDatabase : RoomDatabase() {
             }
 
             return INSTANCE!!
-        }
-
-
-         fun copyAttachedDatabase(context: Context, databaseName: String) {
-            val dbPath: File = context.getDatabasePath(databaseName)
-
-            // If the database already exists, return
-            if (dbPath.exists()) {
-                return
-            }
-
-            // Make sure we have a path to the file
-            dbPath.getParentFile().mkdirs()
-
-            // Try to copy database file
-            try {
-                val inputStream: InputStream = context.assets.open("database/$databaseName")
-                val output: OutputStream = FileOutputStream(dbPath)
-                val buffer = ByteArray(8192)
-                var length: Int=0
-                while (inputStream.read(buffer, 0, 8192).also({ length = it }) > 0) {
-                    output.write(buffer, 0, length)
-                }
-                output.flush()
-                output.close()
-                inputStream.close()
-            } catch (e: IOException) {
-                Log.d("TAG", "Failed to open file", e)
-                e.printStackTrace()
-            }
         }
     }
 }
